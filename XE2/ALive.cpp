cf42a84ad3cf7fc0741ddfe12f798d9f64f46e31
Prof1983 <prof1983@yandex.ru>
2012-08-28 14:29:01 +0600

    Added using ASystem

--- XE2/ALive.cpp
+++ XE2/ALive.cpp
@@ -1,20 +1,31 @@
 /*	ALive
 	Author Prof1983 <prof1983@ya.ru>
 	Created 28.08.2012
-	LastMod 28.08.2012
-*/
+	LastMod 28.08.2012 */
 
 #include <vcl.h>
 //#pragma hdrstop
 #include <tchar.h>
 extern "C" {
 #include "ACore.c"
-#include "APlugins.h"
+#include "ALibraries.c"
+#include "APluginsLibImp.h"
+#include "ASystemLibImp.h"
+
+
+
+
 }
 
 WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
 	CoreLib_Open("ACore32.dll");
+
+	APlugins_Boot(Lib);
+	ASystem_Boot(Lib);
+
+	ASystem_Prepare2A("ALive", "ALive", 0x00000000, "AReason", 12, "AiKernel.org",
+		"(c) AiKernel.org 2012", "http://aikernel.org/", "A", "../Data/");
 
 	ACore_Boot();
 	ACore_Init();
